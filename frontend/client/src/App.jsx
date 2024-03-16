c1f2e90815cbadb0cc452b353298d584e8ead376
dimesha <dimesha99kanchani@gmail.com>
2024-03-16 13:34:46 +0530

    set the react router paths

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import AdminDashboard from './pages/AdminDashboard'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 
 export default function App() {
   return (
-    <h1 className='text-3xl text-red-500'>
-      App
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/dashboard" element={<AdminDashboard />}/>
+        <Route path="/sign-in" element={<SignIn />}/>
+        <Route path="/sign-up" element={<SignUp />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
