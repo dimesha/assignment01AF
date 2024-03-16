31fdda98aa793482c6fc530eedad8a53aed83ef0
dimesha <dimesha99kanchani@gmail.com>
2024-03-16 15:53:55 +0530

    crate the header with responsive

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -4,10 +4,12 @@ import Home from './pages/Home'
 import AdminDashboard from './pages/AdminDashboard'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import Header from './pages/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/dashboard" element={<AdminDashboard />}/>
