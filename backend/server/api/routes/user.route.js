31c14426a02ddaa423e40ad112355a321a00b633
dimesha <dimesha99kanchani@gmail.com>
2024-03-18 18:19:40 +0530

    create test api route

---
+++ backend/server/api/routes/user.route.js
@@ -0,0 +1,12 @@
+import  express  from "express";
+import { test } from "../controllers/user.controller.js";
+
+
+const router = express.Router();
+
+router.get('/test', test);
+
+
+
+
+export default router;
