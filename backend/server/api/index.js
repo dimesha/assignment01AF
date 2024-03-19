306a61fdb4e1ff9fea8263a6c70bc129d6e0e375
dimesha <dimesha99kanchani@gmail.com>
2024-03-19 12:30:01 +0530

    create signup api route with hased password

--- backend/server/api/index.js
+++ backend/server/api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoute from './routes/user.route.js';
+import authRoute  from './routes/auth.route.js';
 
 
 dotenv.config();
@@ -20,9 +21,11 @@ mongoose.connect(
 
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () =>{
     console.log("Server is running on port 3000!!")
 });
 
-app.use('/api/user',userRoute);
+app.use('/api/user', userRoute);
+app.use('/api/auth', authRoute);
--- backend/server/api/model/user.model.js
+++ backend/server/api/model/user.model.js
@@ -13,7 +13,7 @@ const userSchema = new mongoose.Schema({
         unique: true,
 
     },
-    passwors: {
+    password: {
         type: String,
         required: true,
     },
