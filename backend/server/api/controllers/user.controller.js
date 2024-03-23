fadeb6fa1cae946f6c3b71cdf6f16852506d8ffa
dimesha <dimesha99kanchani@gmail.com>
2024-03-23 07:15:18 +0530

    create getallUser function

--- backend/server/api/controllers/user.controller.js
+++ backend/server/api/controllers/user.controller.js
@@ -64,7 +64,7 @@ export const updateUser = async (req, res, next) => {
     }
   };
   
-  // Routee path to Delete User
+  // Router path to Delete User
 export const deleteUser = async (req, res, next) => {
     if (!req.user.isAdmin && req.user.id !== req.params.userId) {
         return next(errorHandler(403, 'Sorry, You can not Delete This User!!...'));
@@ -88,3 +88,44 @@ export const deleteUser = async (req, res, next) => {
       next(error);
     }
   };
+
+  // Router path to Get All User list - only admin can show this list
+export const getUsers = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, 'Sorry,You Are Not Allowed to See All Users!!...'));
+  }
+    try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.sort === 'asc' ? 1 : -1;
+
+    const users = await User.find()
+      .sort({ createdAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+  
+    const usersWithoutPassword = users.map((user) => {
+      const { password, ...rest } = user._doc;
+      return rest;
+    });
+  
+    const totalUsers = await User.countDocuments();
+  
+    const now = new Date();
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+    const lastMonthUsers = await User.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+    res.status(200).json({
+      users: usersWithoutPassword,
+      totalUsers,
+      lastMonthUsers,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
