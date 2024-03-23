6aa1534165e3663e6c8bb8db5860dddc2d6d6eff
dimesha <dimesha99kanchani@gmail.com>
2024-03-23 15:36:48 +0530

    finished get all course function

--- backend/server/api/controllers/course.controller.js
+++ backend/server/api/controllers/course.controller.js
@@ -58,4 +58,49 @@ export const updatecourse = async (req, res, next) => {
       next(error);
     }
   };
+
+//API for Get all Courses
+export const getallcourses = async (req, res, next) => {
+    try {
+      const startIndex = parseInt(req.query.startIndex) || 0;
+      const limit = parseInt(req.query.limit) || 9;
+      const sortDirection = req.query.order === 'asc' ? 1 : -1;
+      const courses = await Course.find({
+        ...(req.query.userId && { userId: req.query.userId }),
+        ...(req.query.courseName && { category: req.query.courseName }),
+        ...(req.query.slug && { slug: req.query.slug }),
+        ...(req.query.courseId && { _id: req.query.courseId }),
+        ...(req.query.searchTerm && {
+          $or: [
+            { courseFaculty: { $regex: req.query.searchTerm, $options: 'i' } },
+            { courseCredit: { $regex: req.query.searchTerm, $options: 'i' } },
+          ],
+        }),
+      })
+        .sort({ updatedAt: sortDirection })
+        .skip(startIndex)
+        .limit(limit);
+    
+      const totalCourses = await Course.countDocuments();  
+      const now = new Date();
+      const oneMonthAgo = new Date(
+        now.getFullYear(),
+        now.getMonth() - 1,
+        now.getDate()
+      );
+    
+      const lastMonthCourses = await Course.countDocuments({
+        createdAt: { $gte: oneMonthAgo },
+      });
+    
+      res.status(200).json({
+        courses,
+        totalCourses,
+        lastMonthCourses,
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+    
   
