c319820efef5d685c6d517a4d2e6f92da3aad5d3
dimesha <dimesha99kanchani@gmail.com>
2024-03-23 17:38:42 +0530

    create notice model

---
+++ backend/server/api/model/notice.model.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const noticeSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      default:"https://firebasestorage.googleapis.com/v0/b/mern-university-management.appspot.com/o/Notification.jpg?alt=media&token=9eb798f9-4c06-4d7f-b16c-2c032e4f205b",
+    },
+    category: {
+      type: String,
+      default: 'uncategorized',
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+{ timestamps: true }
+);
+
+const Notice = mongoose.model('Notice', noticeSchema);
+
+export default Notice;
