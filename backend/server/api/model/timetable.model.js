aecd3bd824826ea0fdb554b709c5d2a760b35295
dimesha <dimesha99kanchani@gmail.com>
2024-03-23 08:50:00 +0530

    create timetable model

---
+++ backend/server/api/model/timetable.model.js
@@ -0,0 +1,45 @@
+/*
+IT21833366
+wijerathna G.D.K
+AF-assignment-01
+restfull-API-UTMS(University Timetable Management System)
+ */
+
+import mongoose from 'mongoose';
+
+const timeTableSchema = new mongoose.Schema({
+    classSession: {
+        type: String,
+        required: true
+    },
+    course: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'courses'
+    },
+    day: {
+        type: Date,
+        required: true
+    },
+    startTime: {
+        type: String,
+        required: true
+    },
+    endTime: {
+        type: String,
+        required: true
+    },
+    faculty: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Users'
+    },
+    location: {
+        type: String,
+        required: true
+    }
+}, 
+{ timestamps: true }
+);
+
+const TimeTable = mongoose.model('TimeTable', timeTableSchema);
+
+export default TimeTable;
